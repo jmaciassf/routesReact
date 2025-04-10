25c3388db01adf3e0f6f90dd1ba19664c5808f9a
Jesus Macias <jesus.macias.sifuentes@banorte.com>
2025-04-10 02:54:59 -0600

    map added

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,9 +1,12 @@
-import { GoogleMap, useJsApiLoader, StandaloneSearchBox } from '@react-google-maps/api'
-import { useRef, useState } from "react";
+import { GoogleMap, useJsApiLoader, StandaloneSearchBox, MarkerF } from '@react-google-maps/api'
+import { useRef, useState, useCallback } from "react";
 
 export function Welcome() {
   function quote(){
     console.log("quote");
+
+    let url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=place_id:"+origin_id+"&destinations=place_id:EiNBdi4gQXN0dXJlcyAxMDgsIDY2NjA3IE4uTC4sIE1leGljbyIwEi4KFAoSCaf-MpYF7WKGERgLIMcItFT6EGwqFAoSCWMk3Z8F7WKGEVdw9jz0oFZz&key="+import.meta.env.VITE_GOOGLEMAPS_API_KEY;
+    console.log(url);
   }
 
   function refresh(){
@@ -14,8 +17,10 @@ export function Welcome() {
     console.log("schedule");
   }
   
+  const [markers, setMarkers] = useState([]);
   const refOrigin = useRef(null);
   const refDestination = useRef(null);
+  const [map, setMap] = useState(null);
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: import.meta.env.VITE_GOOGLEMAPS_API_KEY,
@@ -24,15 +29,88 @@ export function Welcome() {
 
   console.log(isLoaded);
 
+  const [origin_id, setOrigin_id] = useState(null);
   const handleOnPlacesChanged = () => {
     let address = refOrigin.current.getPlaces();
     console.log(address);
+    setOrigin_id(address[0].place_id);
+
+    markerOrigin = { 
+      id: 1, 
+      position: { 
+        lat: address[0].geometry.location.lat(), 
+        lng: address[0].geometry.location.lng()
+      } 
+    }
+
+    updateMarkers();
+  }
+
+  
+  function updateMarkers(){
+    _markers = [];
+    if(markerOrigin)
+      _markers.push(markerOrigin);
+    if(markerDestination)
+      _markers.push(markerDestination);
+
+    setMarkers(_markers);
+    console.log(markers); 
+  }
+
+  const handleOnPlacesChanged2 = () => {
+    let address = refDestination.current.getPlaces();
+    if(address)
+      console.log(address);    
+
+    markerDestination = { 
+      id: 2, 
+      position: { 
+        lat: address[0].geometry.location.lat(), 
+        lng: address[0].geometry.location.lng()
+      } 
+    }
+
+    updateMarkers();
+  }
+
+  const containerStyle = {
+    width: '100%',
+    height: '800px',
   }
   
-  const handleOnPlacesChanged2 = () => {
-    if(refDestination)
-      console.log(refDestination.current.getPlaces());    
+  const center = {
+    lat: 24.593,
+    lng: -101.285
   }
+  
+  let markerOrigin; // = { id: 1, position: { lat: 24.7128, lng: -101.0060 }, name: "A" }
+  let markerDestination; // = { id: 2, position: { lat: 26.7306, lng: -102.9352 }, name: "B" }
+  var _markers = []; // = [ markerOrigin, markerDestination ];
+  //setMarkers(_markers);
+
+  const onLoad = useCallback(function callback(map) {
+    console.log("onLoad");
+
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    //const bounds = new window.google.maps.LatLngBounds(center)
+    //map.fitBounds(bounds)
+
+    setMap(map)
+
+    /*
+    setTimeout(function(){
+      setMarkers(_markers);
+    }, 5000);*/
+    setMarkers(_markers);
+
+  }, [])
+
+  const onUnmount = useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  
 
   return (
     <main>
@@ -55,14 +133,14 @@ export function Welcome() {
                         <StandaloneSearchBox
                           onLoad={(ref) => refOrigin.current = ref}
                           onPlacesChanged={handleOnPlacesChanged}>                        
-                          <input type="text" id="txtOrigin2" placeholder="Origen" />
+                          <input type="text" id="txtOrigin" placeholder="Origen" />
                         </StandaloneSearchBox>
                         }
                         {isLoaded && 
                         <StandaloneSearchBox
                           onLoad={(ref) => refDestination.current = ref}
                           onPlacesChanged={handleOnPlacesChanged2}>                        
-                          <input type="text" id="txtDestination2" placeholder="Destino" />
+                          <input type="text" id="txtDestination" placeholder="Destino" />
                         </StandaloneSearchBox>
                         }
                     </div>
@@ -74,6 +152,30 @@ export function Welcome() {
                 <div id="divResult">
                     Total: $<span id="lblTotal"></span>
                     <input type="button" id="btnSchedule" value="Agendar viaje" onClick={schedule} />
+                </div>
+
+                <div id="divMap">
+                  
+                {isLoaded && 
+                  <GoogleMap
+                  mapContainerStyle={containerStyle}
+                  center={center}
+                  zoom={6}
+                  onLoad={onLoad}
+                  onUnmount={onUnmount}
+                >
+                  {
+                  /* Child components, such as markers, info windows, etc. */
+                  // Coloca AdvancedMarkers en el mapa
+                  markers.map((marker) => (
+                    <MarkerF key={marker.id} position={marker.position}>
+                      
+                    </MarkerF>
+                  ))
+                  }
+                  <></>
+                </GoogleMap>
+                  }
                 </div>
             </div>
         </div>
